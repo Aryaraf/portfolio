9ea6e5cbf1c4098f9116e65ad6d027b11bc91851
Aryaraf <aryaraf711@gmail.com>
2026-07-27 23:35:42 +0700

    fix: update LinkedIn URL in contact card

--- src/App.tsx
+++ src/App.tsx
@@ -651,7 +651,7 @@ function ContactCard() {
     // Tambahkan properti 'url' untuk tujuan link sebenarnya
     { label: 'email',    value: 'aryaryan711@gmail.com',  url: 'mailto:aryaryan711@gmail.com', color: G },
     { label: 'github',   value: 'github.com/Aryaraf',     url: 'https://github.com/Aryaraf', color: T2 },
-    { label: 'linkedin', value: '/in/aryaryan',           url: 'https://linkedin.com/in/aryaryan', color: T2 },
+    { label: 'linkedin', value: '/in/aryaryan',           url: 'https://www.linkedin.com/in/arya-ryan-akbar-fadillah', color: T2 },
     { label: 'whatsapp', value: '+62 8956 0771 7000',     url: 'https://wa.me/62895607717000', color: T2 }, // Langsung buka WA!
   ]
   return (
